7392253490874ae675b69da73fe6c524e7781955
Marek Dworecki <marek.dworecki01@gmail.com>
2025-07-07 15:35:33 +0200

    fix: clear routes

--- src/services/clearRoutes.ts
+++ src/services/clearRoutes.ts
@@ -1,11 +1,36 @@
 import { viewer } from "./displayMap";
 import gsap from "gsap";
 import { stopTracking } from "./userLocation";
+import { upwrBuildingsDataSource } from "./layers";
+import * as Cesium from 'cesium';
 
 const clearRoutes = () => {
     viewer!.entities.removeAll();
     stopTracking()
     gsap.to('#routeClear', {opacity: 0, pointerEvents: 'none', visibility: 'hidden', duration: 0.2});
+    
+
+    
+    upwrBuildingsDataSource?.entities.values.forEach(entity => {
+        entity.polygon!.material = new Cesium.ColorMaterialProperty(Cesium.Color.fromBytes(255, 255, 255, 255));
+        entity.polygon!.outline = new Cesium.ConstantProperty(false);
+        const id = entity.properties?.A._value;
+        if (typeof id === 'string') {
+            if (id.startsWith('A')) {
+                entity.polygon!.material = new Cesium.ColorMaterialProperty(Cesium.Color.fromBytes(135, 70, 36, 255));
+            } else if (id.startsWith('B')) {
+                entity.polygon!.material = new Cesium.ColorMaterialProperty(Cesium.Color.fromBytes(131, 105, 30, 255));
+            } else if (id.startsWith('C')) {
+                entity.polygon!.material = new Cesium.ColorMaterialProperty(Cesium.Color.fromBytes(108, 140, 58, 255));
+            } else if (id.startsWith('D')) {
+                entity.polygon!.material = new Cesium.ColorMaterialProperty(Cesium.Color.fromBytes(52, 171, 116, 255));
+            } else if (id.startsWith('E')) {
+                entity.polygon!.material = new Cesium.ColorMaterialProperty(Cesium.Color.fromBytes(0, 199, 193, 255));
+            } else if (id.startsWith('F')) {
+                entity.polygon!.material = new Cesium.ColorMaterialProperty(Cesium.Color.fromBytes(71, 64, 99, 255));
+            }
+        }
+    });
     
 }
 
