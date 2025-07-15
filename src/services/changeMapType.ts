78f52f4eb128f345369feeec5c6146fe512194e7
Marek Dworecki <marek.dworecki01@gmail.com>
2025-07-15 19:01:34 +0200

    fix: change map type title

--- src/services/changeMapType.ts
+++ src/services/changeMapType.ts
@@ -6,8 +6,10 @@ export const changeMapType = (mapType: string) => {
     if (mapType === '2d'){
         viewer?.destroy()
         createMap()
+        document.title = 'UPWr Wayfinder 2D'
     } else if (mapType === '3d') {
         map.setTarget('')
         displayMap('cesiumContainer')
+        document.title = 'UPWr Wayfinder 3D'
     }
 }
