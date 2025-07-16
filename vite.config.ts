1622af3651df2428965c190cdbe2126823f5150c
Marek Dworecki <marek.dworecki01@gmail.com>
2025-07-16 21:34:20 +0200

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import cesium from 'vite-plugin-cesium';
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify(), cesium()],
+  plugins: [
+    vue(), 
+    vuetify(), 
+    cesium(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
 })
