b308be68f06f482cb22c9ebd17da58488041a54e
Marek Dworecki <marek.dworecki01@gmail.com>
2025-07-16 21:37:05 +0200

    df

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import cesium from 'vite-plugin-cesium';
-import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(), 
     vuetify(), 
-    cesium(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
-    })
+    cesium()
   ],
 })
